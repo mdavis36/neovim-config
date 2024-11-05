bb026278d0ef3bfde0f014dd001b85786096f356
mdavis36 <davis291@llnl.gov>
2024-11-05 15:52:25 -0800

    Switching to lazyvim setup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "Mofiqul/vscode.nvim" },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
+
