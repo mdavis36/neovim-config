6de0c1d42c6efc1402f73954d5c14117bad2f100
mdavis36 <davis291@llnl.gov>
2025-05-20 09:34:26 -0700

    Update to lazyvim v14; Remove neo-tree rnu workaround.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -34,7 +34,7 @@ require("lazy").setup({
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  },                -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
@@ -48,20 +48,6 @@ require("lazy").setup({
         "tutor",
         "zipPlugin",
       },
-    },
-  },
-})
-
--- Need neo-tree to display relativenumber on buffer entry.
-require("neo-tree").setup({
-  event_handlers = {
-    {
-      event = "neo_tree_buffer_enter",
-      handler = function(arg)
-        vim.cmd([[
-          setlocal relativenumber
-        ]])
-      end,
     },
   },
 })
