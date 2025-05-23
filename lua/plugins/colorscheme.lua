cb31756532f9709f3b1848ba99ac7d2d75a0e52b
mdavis36 <michael.l.davis@hotmail.co.uk>
2025-05-23 15:42:08 -0700

    Underline md links.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
   {
     "mdavis36/vscode.nvim",
     branch = "feature/render-md-headers",
+    opts = {
+      underline_links = true,
+    },
   },
 
   -- Configure LazyVim to load colorscheme
