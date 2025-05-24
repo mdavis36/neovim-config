d39998fd446e11b5be5927a333d6798bb543b6fa
mdavis36 <michael.l.davis@hotmail.co.uk>
2025-05-24 12:15:14 -0700

    rnu for neo-tree.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,5 +6,14 @@ return {
         visible = true,
       },
     },
+
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.cmd([[setlocal relativenumber]])
+        end,
+      },
+    },
   },
 }
