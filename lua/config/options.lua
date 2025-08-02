8e407113a1c32bde00d65d5f426792151a2554c8
mdavis36 <michael.l.davis@hotmail.co.uk>
2025-08-01 21:16:44 -0700

    Disable swapfiles.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
+vim.o.wrap = true
+vim.o.swapfile = false
 
 -- Disable LazyVim auto format
 vim.g.autoformat = false
