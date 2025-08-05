bf037b2fa3fd3465425dc7ea48e4f0b831427e02
mdavis36 <michael.l.davis@hotmail.co.uk>
2025-08-05 08:19:22 -0700

    Obsidian: New checkbox.order interface; Use open module.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -188,12 +188,13 @@ return {
       -- vim.ui.open(img, { cmd = { "loupe" } })
     end,
 
-    -- Optional, set to true if you use the Obsidian Advanced URI plugin.
-    -- https://github.com/Vinzent03/obsidian-advanced-uri
-    use_advanced_uri = false,
-
-    -- Optional, set to true to force ':Obsidian open' to bring the app to the foreground.
-    open_app_foreground = false,
+    open = {
+      -- Optional, set to true if you use the Obsidian Advanced URI plugin.
+      -- https://github.com/Vinzent03/obsidian-advanced-uri
+      use_advanced_uri = false,
+      -- Optional, set to true to force ':Obsidian open' to bring the app to the foreground.
+      open_app_foreground = false,
+    },
 
     picker = {
       -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
@@ -257,6 +258,10 @@ return {
       post_set_workspace = function(client, workspace) end,
     },
 
+    checkbox = {
+      order = {" ", "x", ">", "~", "!"},
+    },
+
     -- Optional, configure additional syntax highlighting / extmarks.
     -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
     ui = {
@@ -264,19 +269,6 @@ return {
       update_debounce = 200,  -- update delay after a text change (in milliseconds)
       max_file_length = 5000, -- disable UI features for files with more than this many lines
       -- Define how various check-boxes are displayed
-      checkboxes = {
-        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-        ["x"] = { char = "", hl_group = "ObsidianDone" },
-        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-        ["!"] = { char = "", hl_group = "ObsidianImportant" },
-        -- Replace the above with this if you don't have a patched font:
-        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
-        -- You can also add more custom ones...
-      },
       -- Use bullet marks for non-checkbox lists.
       bullets = { char = "•", hl_group = "ObsidianBullet" },
       external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
