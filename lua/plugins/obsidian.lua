fe560ee3d5f5fba188c6b94d8e02afabb325f7fc
Michael Davis <michael.l.davis@hotmail.co.uk>
2025-11-03 08:31:23 -0800

    Adding default folder for monthly and weekly notes.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -71,18 +71,20 @@ return {
     log_level = vim.log.levels.INFO,
 
     daily_notes = {
-      -- Optional, if you keep daily notes in a separate directory.
       folder = "notes/dailies",
-      -- Optional, if you want to change the date format for the ID of daily notes.
       date_format = "%Y-%m-%d",
-      -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
-      -- Optional, default tags to add to each new daily note created.
       default_tags = { "daily-notes" },
-      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
       template = "calendar/daily.md",
-      -- Optional, if you want `Obsidian yesterday` to return the last work day or `Obsidian tomorrow` to return the next work day.
       workdays_only = false,
+    },
+
+    weekly_notes = {
+      folder = "notes/dailies",
+    },
+
+    monthly_notes = {
+      folder = "notes/dailies",
     },
 
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
