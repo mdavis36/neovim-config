35bf8f205c9f6ae0d7c373ca0d3bab00d33932f9
mdavis36 <michael.l.davis@hotmail.co.uk>
2025-11-18 08:38:55 -0800

    try using osc52 yank method.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,16 @@ vim.g.autoformat = false
 -- Use system clipboard
 vim.o.clipboard = "unnamedplus"
 
+-- Force Neovim to use the OSC 52 clipboard provider
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
